72a5719affa7cb32e91ea5e09c4dce7941e7c849
Michael Gao <mgao4793@gmail.com>
2013-10-13 18:45:47 -0400

    Finish email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Sg::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  user_name: 'app18655078@heroku.com',
+  password: 'mmrceyg2',
+  domain: 'heroku.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
